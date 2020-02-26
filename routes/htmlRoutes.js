dc0ee2e351aaaf098eab3bbf91881f7581c70a0c
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-26 17:08:39 -0600

    Adding routes for API and HTML. HTML Route working

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = function(app) {
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/notes.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
+  });
+};
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, () => {
   console.log("Note Taker App is listening on PORT: http://localhost:" + PORT);
 });

86a8acac738372890056ca08beae10d3b154f59f
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-26 17:34:59 -0600

    updated htmlRoutes to use arrow functions, completed route 1 for apiRoutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,7 @@
-module.exports = function(app) {
-  app.get("/", function(req, res) {
-    res.end("Welcome!");
+const noteData = require("../db/db.json");
+
+module.exports = app => {
+  app.get("/api/notes", (req, res) => {
+    res.json(noteData);
   });
 };

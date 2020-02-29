d29ca9f6a8f1a326aed2bf4948de43d8abf0da8c
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-29 17:41:47 -0600

    Adding delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,8 +10,25 @@ module.exports = app => {
     noteData.push(req.body);
 
     noteData.forEach((note, index) => {
-      note.id = index++;
+      note.id = index + 1;
     });
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteData), err => {
+      if (err) throw err;
+    });
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", (req, res) => {
+    const id = req.params.id;
+    let idx;
+
+    for (let i in noteData) {
+      if (id === noteData[i].id) {
+        idx = i;
+      }
+    }
+    noteData.splice(idx, 1);
 
     fs.writeFile("./db/db.json", JSON.stringify(noteData), err => {
       if (err) throw err;

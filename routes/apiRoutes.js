0c836bec0e286adc5dfd247310aff14ed290e55f
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-29 17:23:03 -0600

    Adding code for post note route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const noteData = require("../db/db.json");
+const fs = require("fs");
 
 module.exports = app => {
   app.get("/api/notes", (req, res) => {
@@ -7,6 +8,14 @@ module.exports = app => {
 
   app.post("/api/notes", (req, res) => {
     noteData.push(req.body);
+
+    noteData.forEach((note, index) => {
+      note.id = index++;
+    });
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteData), err => {
+      if (err) throw err;
+    });
     res.json(true);
   });
 };
